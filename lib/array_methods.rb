90ba7a93e3bb4eb8ad406d0fe53998bd1680a127
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 21:09:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,6 +7,7 @@ zig_zag = [500,4,1000,5,250]
 def find_element_index(array, value_to_find)
     array.length.times do |index|
       array.include?(value_to_find)
+      return array[value_to_find]
     end
 end
 
